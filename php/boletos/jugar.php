faacbdb5713461425883497bbfdcdbc99c491303
Sebastian R <srivero10100@gmail.com>
2023-01-13 14:21:18 -0600

    idk sad

--- php/boletos/jugar.php
+++ php/boletos/jugar.php
@@ -38,31 +38,25 @@ if($ganador_id!=null) {
 
     $call = mysqli_prepare($con, 'CALL usp_boleto_u_ganador(?)');
     mysqli_stmt_bind_param($call, 'i', $ganador_id);
-    mysqli_stmt_execute($call);
+    mysqli_stmt_execute($call);  
+} 
 
-    mysqli_stmt_close($call);
+mysqli_stmt_close($call);
 
-    $call = mysqli_prepare($con, 'CALL usp_boleto_d_ganador(?)');
-    mysqli_stmt_bind_param($call, 'i', $usuario_id);
-    mysqli_stmt_execute($call);
+$call = mysqli_prepare($con, 'CALL usp_boleto_d_ganador(?)');
+mysqli_stmt_bind_param($call, 'i', $usuario_id);
+mysqli_stmt_execute($call);
 
-    mysqli_stmt_close($call);
+mysqli_stmt_close($call);
 
-    $call = mysqli_prepare($con, 'CALL usp_boleto_r_ganador(?, @boleto_ganador)');
-    mysqli_stmt_bind_param($call, 'i', $ganador_id);
-    mysqli_stmt_execute($call);
+$call = mysqli_prepare($con, 'CALL usp_boleto_r_ganador(?, @boleto_ganador)');
+mysqli_stmt_bind_param($call, 'i', $ganador_id);
+mysqli_stmt_execute($call);
 
-    $select = mysqli_query($con, 'SELECT @boleto_ganador');
-    $result = mysqli_fetch_assoc($select);
+$select = mysqli_query($con, 'SELECT @boleto_ganador');
+$result = mysqli_fetch_assoc($select);
 
-    $response['respuesta'] = (string) $result['@boleto_ganador'];
-} else {
-    mysqli_stmt_close($call);
-
-    $call = mysqli_prepare($con, 'CALL usp_boleto_d_ganador(?)');
-    mysqli_stmt_bind_param($call, 'i', $usuario_id);
-    mysqli_stmt_execute($call);
-}
+$response['respuesta'] = (string) $result['@boleto_ganador'];
 
 echo json_encode($response);
 
--- sql/usp_boleto_valida.sql
+++ sql/usp_boleto_valida.sql
@@ -3,21 +3,34 @@ DROP PROCEDURE IF EXISTS usp_boleto_valida;
 delimiter $$
 
 create procedure usp_boleto_valida(
-	in p_usuario_id int,
-	out p_ganador_id int
+	in p_usuario_id int
 )
 begin
+
+	declare v_num_a int;
+	declare v_num_b int;
+	declare v_num_c int;
+	declare v_num_d int;
+	declare v_num_e int;
+	
+	set v_num_a = LPAD(FLOOR(RAND() * 6), 1, '0');
+	set v_num_b = LPAD(FLOOR(RAND() * 6), 1, '0');
+	set v_num_c = LPAD(FLOOR(RAND() * 6), 1, '0');
+	set v_num_d = LPAD(FLOOR(RAND() * 6), 1, '0');
+	set v_num_e = LPAD(FLOOR(RAND() * 6), 1, '0');
+
 	select 
-		b.id into p_ganador_id
+		b.id,
+		CONCAT(v_num_a, " ", v_num_b, " ", v_num_c, " ", v_num_d, " ", v_num_e)
 	from boletos b
 	where
 		b.usuario_id = p_usuario_id
 		and b.estado not in ('inactivo', 'GANADOR')
-		and b.num_a = LPAD(FLOOR(RAND() * 6), 1, '0')
-		and b.num_b = LPAD(FLOOR(RAND() * 6), 1, '0')
-		and b.num_c = LPAD(FLOOR(RAND() * 6), 1, '0')
-		and b.num_d = LPAD(FLOOR(RAND() * 6), 1, '0')
-		and b.num_e = LPAD(FLOOR(RAND() * 6), 1, '0')
+		and b.num_a = v_num_a
+		and b.num_b = v_num_b
+		and b.num_c = v_num_c
+		and b.num_d = v_num_d
+		and b.num_e = v_num_e
 	ORDER BY b.fecha DESC
 	LIMIT 1;
 end$$
